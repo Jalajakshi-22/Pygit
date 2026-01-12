e9af92c578dd95773359de80f3b67ecd6a88edd0
Jalajakshi-22 <jalajakshi1997@gmail.com>
2026-01-12 20:18:41 +0530

    update task_2

--- Task _2.py
+++ Task _2.py
@@ -2,4 +2,6 @@ First_name = input("Enter your first name")
 last_name= input("Enter your last name")
 full_name = First_name + " " + last_name 
 print("Hello, " + full_name + "! Welcome to the program.")
+print("source_data")
+
 

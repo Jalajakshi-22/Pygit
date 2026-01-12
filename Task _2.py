f03fcf75b05664990cb86588c423fa1e88d63785
Jalajakshi-22 <jalajakshi1997@gmail.com>
2026-01-12 20:40:30 +0530

    Cleanup

--- Task _2.py
+++ Task _2.py
@@ -1,4 +1,4 @@
 First_name = input("Enter your first name")
 last_name= input("Enter your last name")
 full_name = First_name + " " + last_name 
-print("Hello, " + full_name + "! Welcome to the program.")
+print("Hello, " + full_name + "! \nWelcome to the program.")

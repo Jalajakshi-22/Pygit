f6bf0de334ec4638874e6e8856cd8570f428060a
Jalajakshi-22 <jalajakshi1997@gmail.com>
2026-01-12 19:51:55 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+num1 = "Enter the first number"
+num2 = "Enter the second number"
+addition= num1 + num2
+subtraction = num1 - num2
+Multiplication = num1 * num2
+division = num1 / num2
+
+print("addition:", addition)
+print("subtraction:",subtraction)
+print("Multiplication:", Multiplication)
+print("division:", division)
+

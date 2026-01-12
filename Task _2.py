abe7f8852ff1aea4aa3ebf1ee38d478ffb78fe93
Jalajakshi-22 <jalajakshi1997@gmail.com>
2026-01-12 20:22:18 +0530

    reverted changes

--- Task _2.py
+++ Task _2.py
@@ -2,6 +2,3 @@ First_name = input("Enter your first name")
 last_name= input("Enter your last name")
 full_name = First_name + " " + last_name 
 print("Hello, " + full_name + "! Welcome to the program.")
-print("source_data")
-
-
